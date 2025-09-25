10c4ef97f13546f9d7f1320d3c31d4b6555cf61c
Leon <pzone618@hotmail.com>
2025-09-25 17:26:34 +0800

    tmp10

--- backend-services/user-service/src/test/java/com/ai/qa/user/api/controller/UserControllerIntegrationTest.java
+++ backend-services/user-service/src/test/java/com/ai/qa/user/api/controller/UserControllerIntegrationTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+
+import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@ActiveProfiles("test")
 public class UserControllerIntegrationTest extends AbstractIntegrationTest {
 
     @Autowired
