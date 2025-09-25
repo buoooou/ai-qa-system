938d312e8507e663796bf206999b65afa1122c6f
Leon <pzone618@hotmail.com>
2025-09-25 18:04:22 +0800

    tmp19

--- backend-services/user-service/src/main/resources/schema.sql
+++ backend-services/user-service/src/main/resources/schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     nickname VARCHAR(255),
-    create_time TIMESTAMP,
-    update_time TIMESTAMP
+    creation_date TIMESTAMP,
+    update_date TIMESTAMP
 );
