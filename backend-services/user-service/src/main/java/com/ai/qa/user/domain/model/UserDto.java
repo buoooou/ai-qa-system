4d127d03fe8eec72beab5408289b2ab82b718f2f
Leon <pzone618@hotmail.com>
2025-09-25 16:30:10 +0800

    tmp05

--- backend-services/user-service/src/main/java/com/ai/qa/user/domain/model/UserDto.java
+++ backend-services/user-service/src/main/java/com/ai/qa/user/domain/model/UserDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserDto {
-    private String userid;
+    private Long id;
     private String username;
     private String password;
     private String nickname;
