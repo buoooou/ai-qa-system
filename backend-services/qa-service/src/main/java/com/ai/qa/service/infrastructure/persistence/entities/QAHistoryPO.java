855ac5948a84b153b358decfa7200785a2d43ddc
sunstone <sunstone1997@163.com>
2025-09-26 20:44:00 +0800

    QAHistoryPO() has protected access

--- backend-services/qa-service/src/main/java/com/ai/qa/service/infrastructure/persistence/entities/QAHistoryPO.java
+++ backend-services/qa-service/src/main/java/com/ai/qa/service/infrastructure/persistence/entities/QAHistoryPO.java
@@ -2,6 +2,7 @@ package com.ai.qa.service.infrastructure.persistence.entities;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class QAHistoryPO {
 
     @Id
