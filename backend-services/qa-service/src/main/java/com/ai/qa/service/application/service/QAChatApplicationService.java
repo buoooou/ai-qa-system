f9ddd3a76ad4e5c9c162bbd4f05bddde1172a9ed
Caroline <fuyanbin2009@163.com>
2025-09-28 19:50:24 +0800

    qa refined

--- backend-services/qa-service/src/main/java/com/ai/qa/service/application/service/QAChatApplicationService.java
+++ backend-services/qa-service/src/main/java/com/ai/qa/service/application/service/QAChatApplicationService.java
@@ -1,12 +1,12 @@
-import reactor.core.publisher.Flux;
 package com.ai.qa.service.application.service;
 
 import com.ai.qa.service.application.dto.ChatCompletionCommand;
 import com.ai.qa.service.application.dto.QAHistoryDTO;
+import reactor.core.publisher.Flux;
 
 public interface QAChatApplicationService {
 
     QAHistoryDTO chat(ChatCompletionCommand command);
 
     Flux<String> chatStream(ChatCompletionCommand command);
-}
+} 
