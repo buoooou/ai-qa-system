c7f2d7a431fd54c26877cd0f7c6e1b2f29b49adc
WEI XU <xuweixw@cn.ibm.com>
2025-09-30 12:51:51 +0800

    repository fix

---
+++ backend-services/user-service/src/main/java/com/ai/qa/user/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ai.qa.user.domain.repository;
+
+import java.util.Optional;
+import com.ai.qa.user.domain.model.User;
+
+public interface UserRepository {
+    void save(User user);
+    Optional<User> findByUsername(String username);
+    Optional<User> findById(Long userId);
+}
